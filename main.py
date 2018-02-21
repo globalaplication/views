bd5307278827ca015c5efff926ebe53cfd0a7aef
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-02-21 06:31:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import urllib
 class youtube(object):
     def __init__(self, channel):
-        self.channel = channel
+        url = "https://www.youtube.com/channel/" +channel+ "/about"
+        object = urllib.urlopen(url)
+        self.source = object.readlines()
     def getviews(self, source):
         for test in range(0, len(source), +1):
             if source[test:test+len("<b>")] == "<b>":
@@ -9,10 +11,7 @@ class youtube(object):
                 end = source.find("</b>", start)
         return "{} izlenme".format(source[start:end])
     def views(self):
-        url = "https://www.youtube.com/channel/" +self.channel+ "/about"
-        object = urllib.urlopen(url)
-        source = object.readlines()
-        for line in source:
+        for line in self.source:
             if line.find('<span class="about-stat">') is not -1 and line.find("views") is not -1 :
                 return self.getviews(line)
 get = youtube("UCRx5NnTE3bK_gMXm0-lKlpw")
